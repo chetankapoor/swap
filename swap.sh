2cd50446ad5a416cf1a93402816351e778ee5086
Chetan Kapoor <chetankapoor321@gmail.com>
2018-01-10 16:32:37 +0100

    Swap.sh updated

--- swap.sh
+++ swap.sh
@@ -8,7 +8,7 @@ if [ ! "$#" -ge 1 ]; then
 	echo "---------------------------------------"
 	echo "Available options:"
 	printf '%s\n' "$@"
-	echo "size - Size of swap"
+	echo "size - Size of swap ( Example - 1G,2G or 1024M)"
 	echo "path - Path to create a swapfile"
     exit 1
 fi
@@ -20,11 +20,21 @@ if [ ! -z "$2" ]; then
     SWAP_PATH=$2
 fi
 
-# Create Swap 
-sudo fallocate -l $SWAP_SIZE $SWAP_FILE
-sudo chmod 600 $SWAP_FILE
-sudo mkswap $SWAP_FILE
-sudo swapon $SWAP_FILE	
-echo "$SWAP_FILE   none    swap    sw    0   0" | sudo tee /etc/fstab -a # Add to fstab
 
+# Checking if swap already exists in ./etc/fstab
+grep -q "swap" /etc/fstab
+if [ $? -ne 0 ]; then
+	sudo fallocate -l $SWAP_SIZE $SWAP_FILE
+	sudo chmod 600 $SWAP_FILE
+	sudo mkswap $SWAP_FILE
+	sudo swapon $SWAP_FILE	
+	echo "$SWAP_FILE   none    swap    sw    0   0" | sudo tee /etc/fstab -a
+else
+	echo 'swapfile found. No changes made.'
+fi
+
+echo '----------------------'
+echo 'Checking list of swap'
+echo '----------------------'
+swapon --show
 
