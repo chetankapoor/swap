530a94125e9bcd21267736560a5bd815981581be
Chetan Kapoor <chetankapoor321@gmail.com>
2018-01-11 11:18:00 +0100

    swap updated

--- swap.sh
+++ swap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
 if [ ! "$#" -ge 1 ]; then
     echo "Usage: $0 {size}"
     echo "(Default path: /var/lib/swap)"
